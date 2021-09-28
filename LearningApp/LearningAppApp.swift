51a2301007686df72dc76e8d6375117cbbe88fed
LawrenceArcher <archer@hey.com>
2021-09-28 08:47:29 +0100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Lawrence Archer on 28/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
